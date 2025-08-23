409ace480306f762daf6c3b25e024221b9e1b564
Zacarias Bendeck <ZacariasBendeck@users.noreply.github.com>
2025-08-23 02:22:56 +0000

    add payroll demo data (employee, rule +100, structure, payslip)

---
+++ addons/x_payroll_demo/__manifest__.py
@@ -0,0 +1,11 @@
+{
+  "name": "Payroll Demo Data",
+  "version": "1.0.0",
+  "license": "AGPL-3",
+  "depends": ["payroll"],  # OCA community payroll
+  "data": [
+    "data/demo_payroll.xml",
+  ],
+  "installable": True,
+}
+
